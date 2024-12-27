34a13b4dc3da6d45f6bf031c87ac607536487919
N70Kai <109056217+N70Kai@users.noreply.github.com>
2024-12-27 14:02:07 -0500

    Split.v

--- fenping.v
+++ fenping.v
@@ -1,4 +1,4 @@
-//分频模块，分频到1Hz
+
 module fenping(
 input clk_50M,
 output reg clk_1Hz
@@ -6,7 +6,7 @@ output reg clk_1Hz
 
 reg [31:0] time_count=32'd0;
 always@(posedge clk_50M)
-	if(time_count==32'd50000000)begin//50M计数50000000次得到1Hz信号,改小为500便于仿真
+	if(time_count==32'd50000000)begin
 		time_count<=32'd0;
 		clk_1Hz<=1;
 		end
